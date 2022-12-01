6fb3e64adcbe638232e8312b8dbd044f271f8cc5
Ben Arthur <benjaminarjun@gmail.com>
2022-11-30 19:32:39 -0800

    Initial commit

---
+++ AdventOfCode2022/Helpers/DataLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Helpers
+{
+    public static class DataLoader
+    {
+        public static string GetDataForDay(int day)
+        {
+            if (day < 1 || day > 25)
+            {
+                throw new Exception($"Cannot fetch data for day {day}; input must be between 1 and 25.");
+            }
+
+            string pathToData = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Data",
+                $"day{day.ToString().PadLeft(2, '0')}.txt"
+            );
+
+            try
+            {
+                return File.ReadAllText(pathToData);
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw new Exception($"Could not find data file {pathToData} for specified day {day}.", ex);
+            }
+        }
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,6 @@
+﻿using AdventOfCode2022.Helpers;
+
+string day1Data = DataLoader.GetDataForDay(1);
+
+Console.WriteLine(day1Data);
+
