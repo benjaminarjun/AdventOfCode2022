38bc9e995f4c812ec8150a5f0a2191b7a8aa9409
Ben Arthur <benjaminarjun@gmail.com>
2022-12-07 18:03:18 -0800

    Move day 4 helper class to its own file

---
+++ AdventOfCode2022/Solvers/Day04/Segment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Solvers.Day04
+{
+    public class Segment
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+
+        public bool Contains(Segment other)
+        {
+            return this.Start <= other.Start && this.End >= other.End;
+        }
+
+        public bool OverlapsWith(Segment other)
+        {
+            return (this.Start <= other.End && this.End >= other.Start);
+        }
+    }
+}
--- AdventOfCode2022/Solvers/Day04/SegmentOverlapFinder.cs
+++ AdventOfCode2022/Solvers/Day04/SegmentOverlapFinder.cs
@@ -48,20 +48,4 @@ namespace AdventOfCode2022.Solvers.Day04
                 .Count();
         }
     }
-
-    public class Segment
-    {
-        public int Start { get; set; }
-        public int End { get; set; }
-
-        public bool Contains(Segment other)
-        {
-            return this.Start <= other.Start && this.End >= other.End;
-        }
-
-        public bool OverlapsWith(Segment other)
-        {
-            return (this.Start <= other.End && this.End >= other.Start);
-        }
-    }
 }
